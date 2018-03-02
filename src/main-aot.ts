dddb2282a3f52eb15df1236246e8276234512189
Chase Gibbons <chase@rentdynamics.com>
2018-03-02 00:05:49 -0700

    build(aot): added main-aot.ts

--- src/main-aot.ts
+++ src/main-aot.ts
@@ -1,11 +1,7 @@
 import {platformBrowser} from '@angular/platform-browser';
 
-import {AppModuleNgFactory} from './app.module.ngfactory';
+import {AppModuleNgFactory} from './app/app.module.ngfactory';
 import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory)
   .catch(err => console.log(err));
